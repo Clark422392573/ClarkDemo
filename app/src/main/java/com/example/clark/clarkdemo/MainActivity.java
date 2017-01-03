23c3e3f5fbab79761c771b55cac92d32ba619fea
zhangxiang <zhaohongsen@chchwk.com>
2017-01-03 10:30:28 +0800

    First commit.

---
+++ app/src/main/java/com/example/clark/clarkdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.clark.clarkdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.clark.clarkdemo.spacetablayout.SpaceTabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    SpaceTabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new FragmentA());
+        fragmentList.add(new FragmentB());
+        fragmentList.add(new FragmentC());
+        fragmentList.add(new FragmentD());
+        fragmentList.add(new FragmentE());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (SpaceTabLayout) findViewById(R.id.spaceTabLayout);
+
+        tabLayout.initialize(viewPager, getSupportFragmentManager(), fragmentList, savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        tabLayout.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+}
---
+++ app/src/main/java/com/example/clark/clarkdemo/spacetablayout/PagerAdapter.java
@@ -0,0 +1,27 @@
+package com.example.clark.clarkdemo.spacetablayout;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+class PagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments;
+
+    PagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+}
